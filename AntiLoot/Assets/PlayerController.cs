476533acd4cc3a508cec92a160f90d37f8e98688
markallman51 <48496758+markallman51@users.noreply.github.com>
2023-02-13 23:31:15 -0500

    coded a jump
    implemented a jump for the player using physics, raycasting, and some bools

--- AntiLoot/Assets/PlayerController.cs
+++ AntiLoot/Assets/PlayerController.cs
@@ -6,8 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     //variables to be adjusted in Unity editor need to be public
     //anything else can be private, as to not crowd the Unity editor
+    //movement
     public float speed;
     
+    //jumping
+    public Rigidbody2D rb;
+    public float jumpForce = 10;
+    public LayerMask ground;
+    public float groundCheckDistance;
+    public Vector3 groundCheckOffset;
+    private bool grounded;
+    private bool jumping = false;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,5 +50,53 @@ public class PlayerController : MonoBehaviour
 
         float xVel = Input.GetAxisRaw("Horizontal");
         transform.Translate(xVel * speed * Time.deltaTime, 0, 0);
+
+        /*
+        JUMPING(this requires different components in different places, but I'll do the whole explanation here)
+        Here we're using a raycast to check if the player is on the ground. When we use the raycast, it'll shoot a ray
+        downwards(we set it to go down) a certain distance(groundCheckDistance). When it shoots, it'll look specifically for the ground,
+        using the "ground" layer(I don't remember why we use layer over tag tho). If the ray hits the ground within the distance, player is 
+        grounded. If false, player is in the air.
+
+        The rays will shoot down from the player sprite. I also have groundCheckOffset, which will be added and substracted from the 
+        center point of the player sprite. I'm doing this so the player hand be hanging off the edge of a platform and still jump,
+        allowing for more precise jumping.
+
+        The rest of the process is kinda simple: 
+            if player is grounded, apply an upward force to the player to jump
+            if not grounded, dont jump.
+        */
+
+        grounded = GroundCheck();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            if(grounded)
+            {
+                jumping = true;
+            }
+        }
+
     }
+
+    private void FixedUpdate()
+    {
+        if (jumping)
+        {
+            rb.velocity = Vector3.up * jumpForce;
+            jumping = false;
+        }
+    }
+
+    private bool GroundCheck()
+    {
+        //params for raycast: start pos, direction, distance, target(ground layer)
+        bool check = (
+            Physics2D.Raycast(transform.position + groundCheckOffset, Vector3.down, groundCheckDistance, ground) ||
+            Physics2D.Raycast(transform.position - groundCheckOffset, Vector3.down, groundCheckDistance, ground));
+
+        return check;
+    }
+
+    
 }
