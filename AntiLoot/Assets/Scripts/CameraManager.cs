504e908da510dfeba329fd2d2d58f330434626ea
Anthony Melendez <57512971+AwesomeFlow@users.noreply.github.com>
2023-03-11 01:46:58 -0500

    Correct Changes

---
+++ AntiLoot/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<CinemachineVirtualCamera>().Follow = player.transform;
+            GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = 3;
+        }
+    }
+}
