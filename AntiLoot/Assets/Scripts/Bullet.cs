bb015186022647ecf4212c4c8ea9982b75bd01cf
Anthony Melendez <57512971+AwesomeFlow@users.noreply.github.com>
2023-04-29 10:33:44 -0400

    Hierarchy clean-up
    Made parents for objects and fixed some minor issues

--- AntiLoot/Assets/Scripts/Bullet.cs
+++ AntiLoot/Assets/Scripts/Bullet.cs
@@ -15,7 +15,7 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Wall"))
+        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag(Tags.PLAYER))
         {
             gameObject.SetActive(false);
         }
